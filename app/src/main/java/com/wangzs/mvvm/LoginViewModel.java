5533f631fa8666e4ec713b38456b52ab10dbab78
wangzhengshuai <wangzs@yuntongxun.com>
2021-07-19 15:41:39 +0800

    错误提交

---
+++ app/src/main/java/com/wangzs/mvvm/LoginViewModel.java
@@ -0,0 +1,13 @@
+package com.wangzs.mvvm;
+
+import com.wangzs.mvvm.base.viewmodel.BaseViewModel;
+
+/**
+ * @Description:
+ * @Author: wangzs
+ * @Date: 6/28/21 4:36 PM
+ * @Version:
+ */
+public class LoginViewModel extends BaseViewModel {
+
+}
